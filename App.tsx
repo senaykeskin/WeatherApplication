869b74027ae350cd1b44fa9b4be449056df0a879
senaykeskin <senay.keskn00@gmail.com>
2023-07-29 23:11:46 +0300

    file structure

---
+++ App.tsx
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import { StyleSheet, View } from "react-native";
+
+function App(): JSX.Element {
+  return (
+    <View>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
