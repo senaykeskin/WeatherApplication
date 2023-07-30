59f98fb6bc8557e601153a0750c846cd20df6b2a
senaykeskin <102466144+senaykeskin@users.noreply.github.com>
2023-07-30 21:02:33 +0300

    navigation was finished (#2)

---
+++ src/navigator/homeNavigator/HomeNavigator.tsx
@@ -0,0 +1,16 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import { Routes } from "../routes";
+import HomeScreen from "../../screens/main/home";
+
+
+const StackMain = createStackNavigator();
+const HomeNavigator = () => {
+  return (
+    <StackMain.Navigator screenOptions={{ gestureEnabled: false }}>
+      <StackMain.Screen name={Routes.Home}
+                        component={HomeScreen} />
+    </StackMain.Navigator>
+  );
+};
+
+export default HomeNavigator;
---
+++ src/screens/main/home/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View } from "react-native";
+import { styles } from "./styles";
+
+const HomeScreen = ({navigation}:any) => {
+  return(
+    <View>
+
+    </View>
+  );
+};
+
+export default HomeScreen;
