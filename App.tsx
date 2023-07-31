37fe3c057500643f98a91ea86232de02954bc0d6
senaykeskin <senay.keskn00@gmail.com>
2023-07-31 14:51:56 +0300

    react-native-vector-icons was intalled

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import HomeNavigator from './src/navigator/homeNavigator/HomeNavigator';
 
--- src/screens/main/home/index.tsx
+++ src/screens/main/home/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import {View} from 'react-native';
 import {styles} from './styles';
+import Icon from "react-native-vector-icons/MaterialIcons"
 
 const HomeScreen = ({navigation}: any) => {
-  return <View></View>;
+  return (
+    <View>
+      <Icon name="add" size={30} color="black" />
+    </View>
+  );
 };
 
 export default HomeScreen;
